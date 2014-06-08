f10b4cb3de28457bb69069c781f624c008e224c5
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2014-06-09 00:41:25 +0900

    add AndroidStaggerdGrid

--- VolleySample/app/src/main/java/com/sample/volleysample/app/MainActivity.java
+++ VolleySample/app/src/main/java/com/sample/volleysample/app/MainActivity.java
@@ -1,19 +1,43 @@
 package com.sample.volleysample.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Response.Listener;
+import com.android.volley.Request.Method;
+import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private RequestQueue mQueue;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mQueue = Volley.newRequestQueue(getApplicationContext());
+        requestVolley();
     }
 
+    private void requestVolley() {
+        // Volleyでリクエスト
+        String url = "http://www.google.com/uds/GnewsSearch?q=Obama&v=1.0";
+        mQueue.add(new JsonObjectRequest(Method.GET, url, null,
+                new Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "response : " + response.toString());
+                    }
+                }, null));
+        mQueue.start();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
